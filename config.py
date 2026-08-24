74e07f3cd013c361731b6c941596333644ab41d4
Hossein <hossein.kh7935@gmail.com>
2026-08-24 19:59:02 +0330

    fix config and model

--- config.py
+++ config.py
@@ -2,7 +2,15 @@ from tokenizer.bpe import SimpleBPE
 tokenizer = SimpleBPE.load("data/tokenizer.json")
 vocab_size = tokenizer.vocab_size
 
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 class Config:
     vocab_size = vocab_size  # 1002
-    embed_dim = 256
+    embed_dim = 768
     block_size = 128
+    num_head = 4
+    num_layer = 4
+    dropout = .3
+    device = device
